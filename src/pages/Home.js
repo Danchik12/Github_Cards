a4d1b7135cdc487dd2c8544ab2d53ac60d991daa
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-08 09:34:19 +0300

    Create Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import {Search} from './../components/Navigation/Search'
+import {Card} from './../components/Card'
 export const Home = () => {
+	const cards=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 	return (
-		<div>
-			<h1>Home page</h1>
-		</div>
+		
+		<>
+			<Search />
+			<div className='row'>
+			{cards.map((card,index) => {
+				return (
+					<div className="col-sm-4 mb-4" key={index}><Card /></div>
+					)
+			})}
+			
+			
+			</div>
+			
+		</>
 
 		)
 }
