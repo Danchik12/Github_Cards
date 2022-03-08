06c6361c9d30071b031c70147f0bde2f476f566e
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-08 09:34:23 +0300

    Create Search.js

---
+++ src/components/Navigation/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export const Search = () =>{
+	return (
+<div className='form-group m-2'>
+<input 
+type='text'
+className='form-control'
+placeholder="Enter Username..."
+
+/>
+
+</div>
+		)
+}
